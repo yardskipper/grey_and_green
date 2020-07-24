8a2a4553951bedd00493e8830dd0050dcd09e7a2
Brian Hodgert <brianhodgert@gmail.com>
2020-07-24 16:19:04 -0500

    practising more

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send("Got to the local copy");
-    console.log('got here');
+    res.send("updated...Got to the local copy");
+    console.log('updated...got here');
 })
 
 app.listen(3000, ()=>{
     
-    console.log('Listening on 3000');
+    console.log('Updated...Listening on 3000');
 })
